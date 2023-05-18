1700041eda8b5afdedaceb2e3b7e92471ca110c5
shajardini <sha.jardini@gmail.com>
2023-05-18 15:59:57 -0300

    atualização

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function App(){
+    return 'Oi minha turma preferida!'
+    
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const elemento = document.getElementById('root')
+
+ReactDOM.render(<App/>, elemento)
+//Render - é para renderizar na tela
+/*recebe 2 parâmetros- 1º o que vai exibir, 2º mostra o elemento que será selecionado*/
+
