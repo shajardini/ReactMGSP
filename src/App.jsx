885da316223b0932592cc6b3676d585a1c0c8a81
shajardini <sha.jardini@gmail.com>
2023-05-19 16:49:24 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Primeiro from "./components/primeiro";
 import ComParametro from "./components/comparametro";
+import DadosPessoais from "./components/dadospessoais";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     return (
 
         <div>
+            <DadosPessoais/>
             <ComParametro titulo="Dia 19/05" subtitulo="Aula React"/>
             <ComParametro titulo="Dia 22/05" subtitulo="Aula de Cidadania"/>
             <p>{mensagem}</p>
---
+++ src/components/dadospessoais/index.jsx
@@ -0,0 +1,10 @@
+function DadosPessoais(){
+    return(
+        <div>
+            <p>Sharlayne Jardini</p>
+            <p>sharlayne@sermais.org.br</p>
+        </div>
+    )
+}
+
+export default DadosPessoais
